b1cf2d5508b21eee8bf906fb5923eb314e501d64
Liwenxiu <742952713@qq.com>
2020-04-01 09:44:33 +0800

    初始化-2

--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 一般要先引入第三方的样式，再引入自己的样式，注意顺序问题，不然会出现自己写的样式被层叠掉的问题
+import './utils/register-vant' // 引入vant组件的js文件
+import './styles/index.less' // 引入自己的样式文件
+
 Vue.config.productionTip = false
 
 new Vue({
---
+++ src/utils/register-vant.js
@@ -0,0 +1,6 @@
+import Vue from 'vue'
+// 加载vant组件
+import { Navbar } from 'vant'
+
+// 注册vant组件
+Vue.use(Navbar)
